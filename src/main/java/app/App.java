c85b56151193312a39926a655e83176ef809c6fb
rubenespinoza1 <r.espinoza05@ufromail.cl>
2020-03-02 14:04:03 -0300

    COMMIT INICIAL

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -11,10 +11,10 @@ public class App {
     public static void main(String[] args) {
         FileMaker f = new FileMaker();
         try {
-            String ruta = "C:\\Users\\maggi\\Documents\\NetBeansProjects\\Proyecto_Estructura_Datos\\src\\data\\pelicula\\peliPrueba.json";
+            String ruta = "C:\\Users\\maggi\\Documents\\NetBeansProjects\\Proyecto_Estructura_Datos\\src\\data\\pelicula\\";
             Pelicula p = new Pelicula("titulo", "director", Categoria.ROMANCE, Pg.TODO_PUBLICO, 0);
-            f.guardarPelicula(p, ruta);
-            Pelicula peliculaObtenida = f.obtenerPelicula(ruta);
+            f.guardarPelicula(p, ruta + "titulo0");
+            Pelicula peliculaObtenida = f.obtenerPelicula(ruta,"titulo",0);
             System.out.println(peliculaObtenida.toString());
         } catch (IOException e) {
             e.printStackTrace();
