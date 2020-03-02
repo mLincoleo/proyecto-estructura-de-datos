069fec8f0cb0c03f026d5dafd5389b059d50258f
usuario <usuario@necesitasMasPilones>
2020-03-02 11:37:19 -0300

    yoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooOOOO

---
+++ src/main/java/logica/Arbol.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author usuario
+ */
+public class Arbol {
+    
+   
+    Node root;
+    
+    public void add(int value) {
+    root = addRecursive(root, value);
+    }
+    
+
+    private Node addRecursive(Node current, int value) {
+    if (current == null) {
+        return new Node(value);
+    }
+ 
+    if (value < current.value) {
+        current.left = addRecursive(current.left, value);
+    } else if (value > current.value) {
+        current.right = addRecursive(current.right, value);
+    } else {
+        // value already exists
+        return current;
+    }
+ 
+    return current;
+}
+    public void traverseInOrder(Node node) {
+    if (node != null) {
+        traverseInOrder(node.left);
+        System.out.print(" " + node.value);
+        traverseInOrder(node.right);
+    }
+    
+    
+}
+    private Node deleteRecursive(Node current, int value) {
+    if (current == null) {
+        return null;
+    }
+ 
+    if (value == current.value) {
+        // Node to delete found
+        // ... code to delete the node will go here
+    } 
+    if (value < current.value) {
+        current.left = deleteRecursive(current.left, value);
+        return current;
+    }
+    current.right = deleteRecursive(current.right, value);
+    return current;
+
+    
+   }
+    
+}
+class Node {
+    int value;
+    Node left;
+    Node right;
+ 
+    Node(int value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+}
+
+
