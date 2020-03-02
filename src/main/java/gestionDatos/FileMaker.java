1c7ecb5e3e6a2710aa3513c6233a4b066eefd6b7
rubenespinoza1 <r.espinoza05@ufromail.cl>
2020-03-02 14:08:33 -0300

    COMMIT INICIAL

--- src/main/java/gestionDatos/FileMaker.java
+++ src/main/java/gestionDatos/FileMaker.java
@@ -18,13 +18,14 @@ public class FileMaker {
     }
     
     public void guardarPelicula(Pelicula pelicula,String ruta) throws IOException{
-        String fileName = 
-        objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(ruta), pelicula);
+        String fileName = pelicula.getTitulo() + pelicula.getId() + ".json";
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(ruta + fileName), pelicula);
         
     }
     
-    public Pelicula obtenerPelicula(String ruta) throws IOException{
-        return objectMapper.readValue(new File(ruta), Pelicula.class);
+    public Pelicula obtenerPelicula(String ruta, String nombrePelicula,int idPelicula) throws IOException{
+        
+        return objectMapper.readValue(new File(ruta + nombrePelicula + idPelicula + ".json"), Pelicula.class);
     }
     
 }
