559540aef1c17b96b4c0df40f96fdbc24b380523
rubenespinoza1 <r.espinoza05@ufromail.cl>
2020-03-05 11:19:14 -0300

    se crea logica para gestionar socios

---
+++ src/main/java/cine/gestionSocios/GestorSocios.java
@@ -0,0 +1,33 @@
+package cine.gestionSocios;
+
+import clientes.Validable;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class GestorSocios implements Validable{
+    
+    private final String dateFormat;
+    
+    public GestorSocios(){
+        this.dateFormat = "dd/MM/yyyy";
+    }
+
+    @Override
+    public boolean validarFechaNacimiento(String fechaNacimiento) {
+        if (fechaNacimiento == null){
+            return false;
+        }
+        DateFormat sdf = new SimpleDateFormat(dateFormat);
+        sdf.setLenient(false);
+        try{
+            sdf.parse(fechaNacimiento);
+        }catch(ParseException e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    
+
+}
