675d90fa27141d71f316c5e5f0acac25940ed7ba
rubenespinoza1 <r.espinoza05@ufromail.cl>
2020-03-05 12:02:54 -0300

    ME EQUIVOQUE, yoooooooo

---
+++ src/main/java/gestionDatos/GestorArchivos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionDatos;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author maggi
+ */
+public interface GestorArchivos {
+    public abstract void guardarDatos(Object objetoAGuardar,String ruta) throws IOException;
+    public abstract Object obtenerDatos(String ruta,String nombreArchivo) throws IOException,FileNotFoundException;
+}
